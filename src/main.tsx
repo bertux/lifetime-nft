96731e4c80ce3958e6fb8c2385d70e27a194f46f
Bertrand Juglas <b@juglas.name>
2023-11-07 17:41:35 +0100

    listing supported chains

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,16 @@ import {
   relayerUrlConst,
   clientIdConst,
 } from "./consts/parameters";
+// import { Ethereum, Polygon } from "@thirdweb-dev/chains";
+import {
+  ArbitrumGoerli,
+  AvalancheFuji,
+  BaseGoerli,
+  BinanceTestnet,
+  Goerli,
+  Mumbai,
+  OptimismGoerli,
+} from "@thirdweb-dev/chains";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -24,13 +34,29 @@ const biconomyApiId =
   urlParams.get("biconomyApiId") || biconomyApiIdConst || "";
 const sdkOptions = getGasless(relayerUrl, biconomyApiKey, biconomyApiId);
 
-const chain = (urlParams.get("chain") && urlParams.get("chain")?.startsWith("{")) ? JSON.parse(String(urlParams.get("chain"))) : urlParams.get("chain") || chainConst;
+const chain =
+  urlParams.get("chain") && urlParams.get("chain")?.startsWith("{")
+    ? JSON.parse(String(urlParams.get("chain")))
+    : urlParams.get("chain") || chainConst;
 
 const clientId = urlParams.get("clientId") || clientIdConst || "";
 
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={chain} sdkOptions={sdkOptions} clientId={clientId}>
+    <ThirdwebProvider
+      activeChain={chain}
+      supportedChains={[
+        ArbitrumGoerli,
+        AvalancheFuji,
+        BaseGoerli,
+        BinanceTestnet,
+        Goerli,
+        Mumbai,
+        OptimismGoerli,
+      ]}
+      sdkOptions={sdkOptions}
+      clientId={clientId}
+    >
       <Toaster />
       <App />
     </ThirdwebProvider>
