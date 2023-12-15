35af633a19ff3318f0f8d965f432891bbf6a1f60
Bertrand Juglas <b@juglas.name>
2023-12-15 16:58:21 +0100

    Revert "remove wallet connection (#39)"
    This reverts commit a8ef447ebb1302a5e596878a15c0e146a22adf02.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {
+  ConnectWallet,
   detectContractFeature,
   useActiveClaimConditionForWallet,
   useAddress,
@@ -12,6 +13,7 @@ import {
   useContractMetadata,
   useNFT,
   useUnclaimedNFTSupply,
+  Web3Button,
   useChainId,
 } from "@thirdweb-dev/react";
 import {
@@ -427,7 +429,7 @@ export default function Home(props: {
             <circle cx={1} cy={1} r={1} />
           </svg> */}
               <strong className="font-semibold">
-                Due to a recent security alert, the usage of this app is paused until further notice. Thanks for your understanding and spread the word!
+                Spread the word and get 15%!
               </strong>
             </p>
             <a
@@ -450,6 +452,7 @@ export default function Home(props: {
           </div>
         </div>
       )}
+      <ConnectWallet className="!absolute !right-4 !top-16" theme={theme} />
       <div className="grid h-screen grid-cols-1 lg:grid-cols-12">
         <div className="hidden h-full w-full items-center justify-center lg:col-span-5 lg:flex lg:px-12">
           <HeadingImage
@@ -575,6 +578,88 @@ export default function Home(props: {
                         +
                       </button>
                     </div>
+                    <Web3Button
+                      contractAddress={
+                        contractQuery.contract?.getAddress() || ""
+                      }
+                      style={{
+                        backgroundColor:
+                          colors[primaryColor as keyof typeof colors] ||
+                          primaryColor,
+                        maxHeight: "43px",
+                      }}
+                      theme={theme}
+                      action={async (cntr) => {
+                        storeReferrerId();
+                        cntr.erc721.claim(quantity);
+                        const config = {
+                          logToConsole: true, // Optional parameter for your debugging purposes
+                          testMode: false, //This tells the SDK to point to our staging environment
+                        };
+                        const client = new ChainvineClient(config);
+                        const campaign = {
+                          id: chainVineCampaignIdConst,
+                        };
+                        const referrerId = getReferrerId();
+                        console.log("address:", address);
+                        console.log("referrerId:", referrerId);
+                        if (address && referrerId) {
+                          const userClient = await client.syncUser(address);
+                          await userClient
+                            .referral({
+                              campaign,
+                            })
+                            .linkToReferrer(referrerId);
+                        }
+                        if (referrerId) {
+                          await client.recordClick(referrerId, campaign.id);
+                        }
+                      }}
+                      isDisabled={!canClaim || buttonLoading}
+                      onError={(err) => {
+                        console.error(err);
+                        console.log({ err });
+                        toast({
+                          title: "Failed to mint drop",
+                          description: (err as any).reason || "",
+                          duration: 9000,
+                          variant: "destructive",
+                        });
+                      }}
+                      onSuccess={() => {
+                        toast({
+                          title: "Successfully registered",
+                          description:
+                            "The NFT will be transferred to your wallet when your transaction is confirmed.",
+                          duration: 5000,
+                          className: "bg-green-500",
+                        });
+                      }}
+                    >
+                      {buttonLoading ? (
+                        <div role="status">
+                          <svg
+                            aria-hidden="true"
+                            className="mr-2 h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="currentColor"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="currentFill"
+                            />
+                          </svg>
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      ) : (
+                        buttonText
+                      )}
+                    </Web3Button>
                   </div>
                 </div>
               )}
@@ -710,15 +795,9 @@ export default function Home(props: {
                 <br />
 
                 <ul className="mb-4 list-disc pl-6">
-                  <li className="text-white">
-                    Limited edition (# per chain, stablecoin needed)
-                  </li>
-                  <li className="text-white">
-                    Arbitrum One (222, buy with USDT)
-                  </li>
-                  <li className="text-white">
-                    Avalanche C-Chain (111, buy with USDT.e)
-                  </li>
+                  <li className="text-white">Limited edition (# per chain, stablecoin needed)</li>
+                  <li className="text-white">Arbitrum One (222, buy with USDT)</li>
+                  <li className="text-white">Avalanche C-Chain (111, buy with USDT.e)</li>
                   <li className="text-white">Base (111, buy with USDC)</li>
                   <li className="text-white">BNB Chain (333, buy with USDT)</li>
                   <li className="text-white">Ethereum (444, buy with USDT)</li>
